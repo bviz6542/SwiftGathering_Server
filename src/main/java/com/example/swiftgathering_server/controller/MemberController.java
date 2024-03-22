77d096d050da224e0e6f02b2c383be72ae801a5b
정준우 <80523328+bviz6542@users.noreply.github.com>
2024-03-22 19:38:29 +0900

    temp

--- src/main/java/com/example/swiftgathering_server/controller/MemberController.java
+++ src/main/java/com/example/swiftgathering_server/controller/MemberController.java
@@ -1,13 +1,12 @@
 package com.example.swiftgathering_server.controller;
 
 import com.example.swiftgathering_server.dto.LoginDto;
+import com.example.swiftgathering_server.security.JwtTokenProvider;
 import com.example.swiftgathering_server.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class MemberController {
 
     private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping ("/members")
     public ResponseEntity<Void> register(@RequestBody LoginDto loginDto) {
@@ -28,18 +28,15 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestBody LoginDto loginDto) {
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
         String id = loginDto.getId();
         String password = loginDto.getPassword();
         try {
             memberService.verify(id, password);
-            return ResponseEntity
-                    .ok()
-                    .build();
+            String token = jwtTokenProvider.createToken(id);
+            return ResponseEntity.ok(token);
         } catch (Exception e) {
-            return ResponseEntity
-                    .status(HttpStatusCode.valueOf(401))
-                    .build();
+            return ResponseEntity.status(401).body("Unauthorized");
         }
     }
 }
---
+++ src/main/java/com/example/swiftgathering_server/security/JwtTokenProvider.java
@@ -0,0 +1,27 @@
+package com.example.swiftgathering_server.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.util.Date;
+
+@Service
+public class JwtTokenProvider {
+
+    @Value("${JWT_SECRET_KEY}")
+    private String secretKey;
+    private final long validityInMilliseconds = 3600000;
+
+    public String createToken(String username) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+}
