489230539d6b05700bfbe366a887b202ec7fb7ea
정준우 <80523328+bviz6542@users.noreply.github.com>
2024-03-27 23:59:51 +0900

    fix member (#7)

--- src/main/java/com/example/swiftgathering_server/controller/MemberController.java
+++ src/main/java/com/example/swiftgathering_server/controller/MemberController.java
@@ -3,13 +3,13 @@ package com.example.swiftgathering_server.controller;
 import com.example.swiftgathering_server.dto.LoginDto;
 import com.example.swiftgathering_server.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.naming.AuthenticationException;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,27 +19,23 @@ public class MemberController {
 
     @PostMapping ("/members")
     public ResponseEntity<Void> register(@RequestBody LoginDto loginDto) {
-        String id = loginDto.getId();
-        String password = loginDto.getPassword();
-        memberService.register(id, password);
-        return ResponseEntity
-                .ok()
-                .build();
+        memberService.register(loginDto.getId(), loginDto.getPassword());
+        return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/login")
     public ResponseEntity<Void> login(@RequestBody LoginDto loginDto) {
-        String id = loginDto.getId();
-        String password = loginDto.getPassword();
         try {
-            memberService.verify(id, password);
-            return ResponseEntity
-                    .ok()
-                    .build();
-        } catch (Exception e) {
-            return ResponseEntity
-                    .status(HttpStatusCode.valueOf(401))
-                    .build();
+            memberService.verify(loginDto.getId(), loginDto.getPassword());
+            return ResponseEntity.ok().build();
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<String> handleAuthenticationException(AuthenticationException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authentication failed");
     }
 }
