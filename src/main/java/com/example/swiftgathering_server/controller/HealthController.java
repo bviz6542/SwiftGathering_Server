7902174ecae2cc41333f61c9a6a81327f50bec3c
정준우 <80523328+bviz6542@users.noreply.github.com>
2024-07-28 21:03:38 +0900

    readme-for-app-demo-gif (#43)
    * feature: readme
    
    * fix: git actions rmq settings
    
    * fix: delete previos container via docker stop
    
    * feature: ApplicationStartup PostConstruct Instant
    
    * fix: build and push first
    
    * fix: docker image name
    
    * fix: gradlew build
    
    * fix: build in local
    
    * fix: remove docker build from script

--- src/main/java/com/example/swiftgathering_server/controller/HealthController.java
+++ src/main/java/com/example/swiftgathering_server/controller/HealthController.java
@@ -1,15 +1,24 @@
 package com.example.swiftgathering_server.controller;
 
+import com.example.swiftgathering_server.config.ApplicationStartup;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 @RequestMapping("/health")
 public class HealthController {
     @GetMapping
     public ResponseEntity<String> checkHealth() {
-        return ResponseEntity
-                .ok("v1.0.6");
+        Instant bootTime = ApplicationStartup.getStartTime();
+        ZonedDateTime bootTimeKST = bootTime.atZone(ZoneId.of("Asia/Seoul"));
+        String bootTimeFormatted = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z").format(bootTimeKST);
+        String response = String.format("boot time: %s", bootTimeFormatted);
+        return ResponseEntity.ok(response);
     }
 }
