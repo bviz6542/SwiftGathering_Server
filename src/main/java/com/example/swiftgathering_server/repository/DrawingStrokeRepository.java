95e6f41597e75f2394693713ac5bcadf9c316f53
정준우 <80523328+bviz6542@users.noreply.github.com>
2024-07-30 01:49:53 +0900

    refactor: findAllFriendsOfUser query

--- src/main/java/com/example/swiftgathering_server/repository/DrawingStrokeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.swiftgathering_server.repository;
-
-import com.example.swiftgathering_server.domain.DrawingStroke;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DrawingStrokeRepository extends JpaRepository<DrawingStroke, Long> {
-}
